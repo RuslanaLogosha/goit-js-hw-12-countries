1fb1c2fd23a261af4a588cff4417b3b56bfbfbaf
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-10-31 11:09:46 +0200

    main job is done

--- src/index.js
+++ src/index.js
@@ -4,26 +4,14 @@ import getRefs from './js/get-refs'
 import API from './js/api-service'
 import countryTempOne from './templateOne.hbs';
 import countryTemp2_10 from './template2_10.hbs'
-// import { alert } from '../node_modules/@pnotify/core/dist/PNotify.js';
-import { alert } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
+import { defaults } from '@pnotify/core';
 
-
-
-const myAlert = alert({
-  text: "Too many matches found. Please enter more specific query.",
-  type: 'notice',
-  styling: 'brighttheme',
-});
 
 const refs = getRefs();
 
 refs.searchInput.addEventListener('input', debounce(onSearch, 500));
-
-
-//   API.fetchCountry(searchQuery).
-//   then(renderCountryCard).catch(onCatchError).finally(onResetInput)
-// }
 
 function onSearch(event) {
   const searchQuery = refs.searchInput.value;
@@ -36,14 +24,21 @@ function renderCountryCard(countries) {
      const markup = countryTempOne(countries[0]);
      refs.countriesCountainer.innerHTML = markup;
   }
-  if (countries.length >= 2 || countries.length <= 10) {
+  if (countries.length >= 2 && countries.length <= 10) {
     const markup2_10 = countryTemp2_10(countries);
     refs.countriesCountainer.innerHTML = markup2_10;
   }
 
-  else if (countries.length > 10) {
-    
-    alert();
+  if (countries.length >= 11) {
+
+    defaults.width = '400px';
+    defaults.delay = 700;
+    defaults.addClass = 'errorStyle';
+
+    const myError = error({
+    text: "Too many matches found. Please enter a more specific query."
+  });
+
   }
  
 }
